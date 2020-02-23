927d79e20681c8b88c1407b3acac0bfbf3c9d0de
unknown <contact@itok01.com>
2020-02-23 10:52:42 +0900

    Add postUserLocation()

--- api/api.js
+++ api/api.js
@@ -1,7 +1,18 @@
+function getQueryParam(key) {
+    // 1.URLから (指定したパラメータ名~&|$|#) で終わる箇所を抜き出し
+    const value = window.location.href.match(new RegExp("[?&]" + key + "=(.*?)(&|$|#)"));
+    // 2.パラメータ名に一致する値が存在しない場合は空返却
+    if (value == null) return '';
+    // 3.パラメータ名に一致する値が存在した場合はURLデコードして返却
+    return decodeURIComponent(value[1]);
+}
+
+let user = getQueryParam("user");
+
 const API_ENDPOINT = "https://httpbin.org"
 
 function getEvent(eventID) {
-    fetch(API_ENDPOINT + "/v1/event?event_id=" + eventID)
+    fetch(API_ENDPOINT + "/v1/event?user=" + user + "&event_id=" + eventID)
         .then(res => res.json())
         .catch(error => console.error('Error:', error));
 }
@@ -49,7 +60,7 @@ function joinEvent(eventID) {
 }
 
 function getNgDate(eventID) {
-    fetch(API_ENDPOINT + "/v1/event/ngdate?event_id=" + eventID)
+    fetch(API_ENDPOINT + "/v1/event/ngdate?user=" + user + "&event_id=" + eventID)
         .then(res => res.json())
         .catch(error => console.error('Error:', error));
 }
@@ -69,13 +80,13 @@ function postNgDate(eventID) {
 }
 
 function getDepatureTime(eventID) {
-    fetch(API_ENDPOINT + "/v1/event/departure?event_id=" + eventID)
+    fetch(API_ENDPOINT + "/v1/event/departure?user=" + user + "&event_id=" + eventID)
         .then(res => res.json())
         .catch(error => console.error('Error:', error));
 }
 
 function getRoute(eventID) {
-    fetch(API_ENDPOINT + "/v1/event/route?event_id=" + eventID)
+    fetch(API_ENDPOINT + "/v1/event/route?user=" + user + "&event_id=" + eventID)
         .then(res => res.json())
         .catch(error => console.error('Error:', error));
 }
@@ -95,27 +106,29 @@ function arrivalEvent(eventID) {
 }
 
 function getArrivalRank(eventID) {
-    fetch(API_ENDPOINT + "/v1/event/arrival/rank?event_id=" + eventID)
+    fetch(API_ENDPOINT + "/v1/event/arrival/rank?user=" + user + "&event_id=" + eventID)
         .then(res => res.json())
         .catch(error => console.error('Error:', error));
 }
 
-function getUserLocation(user) {
-    fetch(API_ENDPOINT + "/v1/user/location?user=" + user)
+function getUserLocation(targetUser) {
+    fetch(API_ENDPOINT + "/v1/user/location?user=" + user + "&target_user=" + targetUser)
         .then(res => res.json())
         .catch(error => console.error('Error:', error));
 }
 
-function postUserLocation(lat, lon) {
-    let data = { lat: lat, lon: lon };
+function postUserLocation() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        let data = { lat: position.coords.latitude, lon: position.coords.longitude };
 
-    fetch(API_ENDPOINT + "/v1/user/location", {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(res => res.json())
-        .catch(error => console.error('Error:', error));
+        fetch(API_ENDPOINT + "/v1/user/location", {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .catch(error => console.error('Error:', error));
+    });
 }
