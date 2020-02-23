9a0f2176d6ddfcc687078eee218186713915d8ae
canipoyo <canipoyoict@gmail.com>
2020-02-23 11:43:49 +0900

    [add]guest maps

---
+++ guestmaps/index.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var latlng = new google.maps.LatLng(34.808502, 135.639683);//中心の緯度, 経度
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,//ズームの調整
+    center: latlng//上で設定した中心
+  });
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map
+  });
+}
