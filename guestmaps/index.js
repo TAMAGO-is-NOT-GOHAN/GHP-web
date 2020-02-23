cfc88b8e73a0213cb9405bfe50cef23263144be7
unknown <contact@itok01.com>
2020-02-23 14:57:51 +0900

    Fix

--- guestmaps/index.js
+++ guestmaps/index.js
@@ -11,7 +11,7 @@ eventDate.textContent = resp["date"];
 eventDescription.textContent = resp["event-description"];
 
 function initMap() {
-  var latlng = new google.maps.LatLng(34.808502, 135.639683);//中心の緯度, 経度
+  var latlng = new google.maps.LatLng(26.314336, 127.796112);//中心の緯度, 経度
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,//ズームの調整
     center: latlng//上で設定した中心
