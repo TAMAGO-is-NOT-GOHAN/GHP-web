ee870306308241714df35ca53eeacd86de6f86fc
unknown <contact@itok01.com>
2020-02-23 14:51:24 +0900

    Fix

--- maps/index.js
+++ maps/index.js
@@ -1,5 +1,15 @@
 postUserLocation();
 
+let eventName = document.getElementById("event-name");
+let eventDate = document.getElementById("date");
+let eventDescription = document.getElementById("event-description");
+
+let resp = getQueryParam("event-id");
+
+eventName.textContent = resp["event-name"];
+eventDate.textContent = resp["date"];
+eventDescription.textContent = resp["event-description"];
+
 function initMap() {
   var latlng = new google.maps.LatLng(34.808502, 135.639683);//中心の緯度, 経度
   var map = new google.maps.Map(document.getElementById('map'), {
