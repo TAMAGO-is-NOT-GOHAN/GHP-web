c4254a5f9b81aa502884bdeefb4b01422b69e552
ebina0123 <ebina1105@gmail.com>
2020-02-23 15:04:18 +0900

    [add Join Event

--- guest/index.js
+++ guest/index.js
@@ -1,7 +1,9 @@
-document.getElementById('event_id').textContent = "ルームID : " + getQueryParam("event_id")
-postUserLocation();
+var eventID = getQueryParam('event_id')
+document.getElementById('event_id').textContent = "ルームID : " + eventID
+postUserLocation()
 
 function PostUsernameAndEventID() {
   var userName = Userform.username.value;
-  location.href = '../guestmaps/index.html' + "?username=" + userName + "&event_id=" + getQueryParam('event_id')
+  joinEvent(eventID)
+  location.href = '../guestmaps/index.html' + "?username=" + userName + "&event_id=" + eventID
 }
