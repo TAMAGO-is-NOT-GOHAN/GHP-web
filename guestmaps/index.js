1f3bba08b8ee4f2ab737028c71bd30b2fa6aafe1
canipoyo <canipoyoict@gmail.com>
2020-02-23 13:51:51 +0900

    [add]tyre

--- guestmaps/index.js
+++ guestmaps/index.js
@@ -1,17 +1,3 @@
-postUserLocation();
-
-function initMap() {
-  var latlng = new google.maps.LatLng(34.808502, 135.639683);//中心の緯度, 経度
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,//ズームの調整
-    center: latlng//上で設定した中心
-  });
-  var marker = new google.maps.Marker({
-    position: latlng,
-    map: map
-  });
-}
-
 let form = document.getElementById("nameform");
 
 let eventName = document.getElementById("event-name").value;
